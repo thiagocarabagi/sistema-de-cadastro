5d58101a79e6e2cfcdcbd0800c96abadb812b6ea
Usuario Thiago <thiagooo_carabagi@hotmail.com>
2025-03-31 17:29:34 -0300

    voltando para um estado anterior

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log para teste:
 console.log("O Thiago fez esse commit sem voce ver a aula")
-console.log("Enviando para o GitHub")
