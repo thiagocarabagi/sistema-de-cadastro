4e1e9e8c855a8333ca928475e7ad38f7a6d895e3
Usuario Thiago <thiagooo_carabagi@hotmail.com>
2025-03-31 17:07:19 -0300

    acrescentei mais linha no app js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log para teste:
+console.log("O Thiago fez esse commit sem voce ver a aula")
